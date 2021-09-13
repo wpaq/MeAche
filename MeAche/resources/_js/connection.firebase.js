99df2ccfafb1ec9c75819dc73cc578b42aa87b19
Wallyson Pablo <42584214+wpaq@users.noreply.github.com>
2021-09-13 20:55:56 -0300

    Update connection.firebase.js

--- MeAche/resources/_js/connection.firebase.js
+++ MeAche/resources/_js/connection.firebase.js
@@ -1,17 +1,15 @@
 // Initialize Firebase
 const firebaseConfig  = {
-  apiKey: "AIzaSyA8fBbF_xn3q33YdNWY2oDCQ3D9QpNFPO4",
-  authDomain: "me-ache-6c62e.firebaseapp.com",
-  databaseURL: "https://me-ache-6c62e.firebaseio.com",
-  projectId: "me-ache-6c62e",
-  storageBucket: "me-ache-6c62e.appspot.com",
-  messagingSenderId: "164197814660"
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  databaseURL: "databaseURL",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId"
 };
 // Initialize your Firebase app
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 function getReference() {
-  // [START rtdb_get_reference]
   return database = firebase.database();
-  // [END rtdb_get_reference]
 }
